a39a896af94f9703e583af879d1fb13b06ad2c73
Suman Kumari <sumanbijarniya5@gmail.com>
2021-02-28 14:36:06 +0530

    adding new components: collection-preiew and collection-item and add a shop page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { Switch, Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.jsx";
-
-const HatsPage = () => {
-  return(
-    <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  ); 
-}
+import ShopPage from "./pages/shop/shop.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
